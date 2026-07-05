8a4227033787d09099726b972dfeec7da5344f62
faredmansour <faredmansour6@gmail.com>
2026-07-05 06:41:54 +0200

    firest

--- educommunity-backend/src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const ctrl = require('../controllers/auth.controller');
-const { authenticate } = require('../middleware/auth.middleware');
-
-router.post('/register', ctrl.register);
-router.post('/login', ctrl.login);
-router.get('/me', authenticate, ctrl.me);
-
-module.exports = router;
